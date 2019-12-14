5450486c8ff334148d3094a81033b99c5d7be2e2
jesualdorsdmatos <a21250379@isec.pt>
2019-12-14 01:08:31 +0000

    Hiperparamets resolvidos

---
+++ teste.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 22:14:27 2019
+
+Binary PSO
+
+Feature Selection
+
+https://pyswarms.readthedocs.io/en/latest/examples/usecases/feature_subset_selection.html
+
+"""
+
+
+# Import modules
+import numpy as np
+import random
+
+# Import PySwarms
+import pyswarms as ps
+
+RANDOM_SEED = 42
+random.seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)
+
+
+"""
+toy dataset, 
+10 features,
+5 are informative,
+5 are redundant,
+2  are repeated
+"""
+
+from sklearn.datasets import make_classification
+X, y = make_classification(n_samples=100, n_features=15, n_classes=3,
+                           n_informative=4, n_redundant=1, n_repeated=2,
+                           random_state=1)
+
+"""
+Definição do classifcador 
+"""
+from sklearn import linear_model
+classifier = linear_model.LogisticRegression()
+
+"""
+Função objetivo
+"""
+
+def f_per_particle(m, alpha):
+    """Computes for the objective function per particle
+
+    Inputs
+    ------
+    m : numpy.ndarray
+        Binary mask that can be obtained from BinaryPSO, will
+        be used to mask features.
+    alpha: float (default is 0.5)
+        Constant weight for trading-off classifier performance
+        and number of features
+
+    Returns
+    -------
+    numpy.ndarray
+        Computed objective function
+    """
+    total_features = 15
+    # Get the subset of the features from the binary mask
+    if np.count_nonzero(m) == 0:
+        X_subset = X
+    else:
+        X_subset = X[:,m==1]
+    # Perform classification and store performance in P
+    classifier.fit(X_subset, y)
+    P = (classifier.predict(X_subset) == y).mean()
+    # Compute for the objective function NOTA VER ISTO
+    j = (alpha * (1.0 - P)
+        + (1.0 - alpha) * (1 - (X_subset.shape[1] / total_features)))
+
+    return j
+
+
+def f(x, alpha=0.88):
+    """Higher-level method to do classification in the
+    whole swarm.
+
+    Inputs
+    ------
+    x: numpy.ndarray of shape (n_particles, dimensions)
+        The swarm that will perform the search
+
+    Returns
+    -------
+    numpy.ndarray of shape (n_particles, )
+        The computed loss for each particle
+    """
+    n_particles = x.shape[0]
+    j = [f_per_particle(x[i], alpha) for i in range(n_particles)]
+    return np.array(j)
+
+
+"""
+Binary PSO
+"""
+
+# Initialize swarm, arbitrary
+options = {'c1': 0.5, 'c2': 0.5, 'w':0.9, 'k': 30, 'p':2}
+
+# Call instance of PSO
+dimensions = 15 # dimensions should be the number of features
+optimizer = ps.discrete.BinaryPSO(n_particles=30, dimensions=dimensions, options=options)
+
+cost, pos = optimizer.optimize(f, iters=1000)
+
+"""
+Desempenho do classificador
+"""
+# Create two instances of LogisticRegression
+classifier = linear_model.LogisticRegression()
+# Get the selected features from the final positions
+X_selected_features = X[:,pos==1]  # subset
+# Perform classification and store performance in P
+classifier.fit(X_selected_features, y)
+# Compute performance
+subset_performance = (classifier.predict(X_selected_features) == y).mean()
+print('Subset performance: %.3f' % (subset_performance))
+
+
+
+"""
+Desempenho do classificador completo
+"""
+classifierDois = linear_model.LogisticRegression()
+classifierDois.fit(X, y)
+performance = (classifierDois.predict(X) == y).mean()
+print('Complete set performance: %.3f' % (performance))
--- treino.py
+++ treino.py
@@ -19,45 +19,46 @@ from SwarmPackagePy import testFunctions as tf
 from SwarmPackagePy import animation, animation3D
 ########## FIM DOS IMPORTS##########
  
-########## INICIO DO LER FICHEIRO##########
-df = pd.read_csv('aadr.us.txt', names=["Data","Open","High","Low","Close"], usecols=[0,1,2,3,4] , parse_dates=True, index_col=0)
-########## FiM DO LER FICHEIRO##########
+
+
+
+########## INICIO DO TRATAMENTO DE DADOS ########## 
+tabela = pd.read_table('goog.us.txt', skiprows=1, sep = ',', names = ['Date', 'Open','High','Low','Close','Volume'],  usecols = [0,1,2,3,4,5], parse_dates = True, index_col = 0)
+
+#shuffle para ler dados de forma aleatoria. tem que estar a false para não acontecer
+train_input_data, test_input_data, train_target_data, test_target_data = train_test_split(tabela, tabela['Open'], test_size=.3, shuffle = False)
+
+#Treino
+train_input_data = np.array(train_input_data[1:])
+train_target_data = np.array(train_target_data[0:(len(train_target_data)-1)])
+
+#Teste
+test_input_data = np.array(test_input_data[1:])
+test_target_data = np.array(test_target_data[0:(len(test_target_data)-1)])
+train_target_data = np.reshape(train_target_data,(len(train_target_data),1))
+test_target_data = np.reshape(test_target_data,(len(test_target_data),1))
+scaler = MinMaxScaler(feature_range=(0, 1))
 
  
-########## INICIO DO TRATAMENTO DE DADOS ########## 
-treino_input, teste_input, treino_target, teste_target = train_test_split(df, df['Open'] , test_size=0.2 , shuffle=False)
- 
- 
-treino_target = np.array(treino_target[1:])
-teste_input =  np.array(teste_input[0:(len(teste_input)-1)])
-treino_input = np.array(treino_input[0:(len(treino_input)-1)])
-teste_target = np.array(teste_target[1:])
- 
-treino_target = np.reshape(treino_target,(len(treino_target),1))
- 
-teste_target = np.reshape(teste_target,(len(teste_target),1))
+#Normalizar
+train_input_data = scaler.fit_transform(train_input_data)
+train_target_data = scaler.fit_transform(train_target_data)
+test_input_data = scaler.fit_transform(test_input_data)
+test_target_data = scaler.fit_transform(test_target_data)
 ########## FIM DO TRATAMENTO DE DADOS ########## 
 
  
-########## INICIO DE NORMALIZACAO ########## 
-scaler = MinMaxScaler(feature_range=(0, 1))
-treino_target = scaler.fit_transform(treino_target)
-teste_input = scaler.fit_transform(teste_input)
-t_tratreino_inputin = scaler.fit_transform(treino_input)
-teste_target = scaler.fit_transform(teste_target)
-########## FIM DE NORMALIZACAO ########## 
- 
- 
+
+
 
 ########## INICIO DA FUNCAO MLPREGRESS ##########  
-def mlp_regress(hyperparamets):
-    regress = MLPRegressor(hidden_layer_sizes=(80,) , activation='logistic', 
-    solver='adam', batch_size='auto', learning_rate_init=(hyperparamets))
-    regress.fit(treino_input,treino_target.ravel())
-    regress.predict(teste_input)
-    score = regress.score(teste_input,teste_target)
+def mlp_regress(hyperparameters):
+    model = MLPRegressor(activation = 'logistic', hidden_layer_sizes=(150,),learning_rate_init=(hyperparameters[0]), momentum = (hyperparameters[1]))
+    model.fit(train_input_data, train_target_data)
+    model.predict(test_input_data)
+    score = model.score(train_input_data, train_target_data)
+    print(); print(model)
     loss = (1-score)
-    print(regress)
     print(loss)
     return loss
 ########## FIM DA FUNCAO MLPREGRESS ##########  
@@ -67,16 +68,20 @@ def mlp_regress(hyperparamets):
 ########## INICIO DA FUNCAO DE ENVIO ##########  
 def f(x):
     n_particles = x.shape[0]
-    j = [mlp_regress(x[i]) for i in range(n_particles)]
+    for i in [range(n_particles)]:
+        valor=x[i]
+          
+    j = mlp_regress(valor)
     return np.array(j)
 ########## FIM DA FUNCAO DE ENVIO ##########  
 
 
 ########## INICIO DA CHAMADA DA FUNCAO CAT ##########   
-alh = sp.ca(10, f , 0.2 , 1.0, 1 ,5, mr=4, smp=2,
-                 spc=False, cdc=1, srd=0.1, w=0.1, c=0.5, csi=0.6)
+alh = sp.ca(10, f , 0.0001 , 0.9, 2 ,2, mr=10, smp=2,
+                 spc=False, cdc=1, srd=0.1, w=0.1, c=1.05, csi=0.6)
 ########## FIM DA CHAMADA DA FUNCAO CAT ##########   
 
 print ("best score :",alh.get_Gbest())
-print("h:",alh.get_agents())
-#animation3D(alh.get_agents(), f, 10, -10, sr=True)
+#print("h:",alh.get_agents())
+mlp_regress(alh.get_Gbest())
+#animation3D(alh.get_agents(), f, 10,10, sr=True)
