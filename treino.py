f867a28834ca7c0c4253ff40a2e2ff852762f1d0
jesualdorsdmatos <a21250379@isec.pt>
2019-12-15 18:02:05 +0000

    Adicionado grafico

--- treino.py
+++ treino.py
@@ -5,7 +5,7 @@
  
  
  
-########## INICIO DOS IMPORTS##########
+################################ INICIO DOS IMPORTS ################################
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
@@ -16,97 +16,58 @@ import matplotlib.pyplot as plt
 import pyswarms as ps
 import SwarmPackagePy as sp
 from SwarmPackagePy import testFunctions as tf
-
-########## FIM DOS IMPORTS##########
+from pyswarms.utils.functions import single_obj as fx
+from pyswarms.utils.plotters.formatters import Mesher
+from pyswarms.utils.plotters import (plot_cost_history, plot_contour, plot_surface)
+from matplotlib import animation, rc
+from IPython.display import HTML
+################################ FIM DOS IMPORTS ################################
  
-#####################TESTE########################################################3
-from matplotlib import pyplot as plt
-import matplotlib.animation
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-import pandas as pd
-
-
-def animation(agents, function, lb, ub, sr=False):
-
-    side = np.linspace(lb, ub, (ub - lb) * 5)
-    X, Y = np.meshgrid(side, side)
-    Z = np.array([np.array([function([X[i][j], Y[i][j]])
-                            for j in range(len(X))])
-                  for i in range(len(X[0]))])
-
-    fig = plt.figure()
-    plt.axes(xlim=(lb, ub), ylim=(lb, ub))
-    plt.pcolormesh(X, Y, Z, shading='gouraud')
-    plt.colorbar()
-
-    x = np.array([j[0] for j in agents[0]])
-    y = np.array([j[1] for j in agents[0]])
-    sc = plt.scatter(x, y, color='black')
-
-    plt.title(function.__name__, loc='left')
-
-    def an(i):
-        x = np.array([j[0] for j in agents[i]])
-        y = np.array([j[1] for j in agents[i]])
-        sc.set_offsets(list(zip(x, y)))
-        plt.title('iteration: {}'.format(i), loc='right')
-
-    ani = matplotlib.animation.FuncAnimation(fig, an, frames=len(agents) - 1)
-
-    if sr:
-
-        ani.save('result.mp4')
 
 
 
-####################FIM TESTE######################################################
-
-
-########## INICIO DO TRATAMENTO DE DADOS ########## 
+################################ INICIO DO TRATAMENTO DE DADOS ################################
 tabela = pd.read_table('goog.us.txt', skiprows=1, sep = ',', names = ['Date', 'Open','High','Low','Close','Volume'],  usecols = [0,1,2,3,4,5], parse_dates = True, index_col = 0)
 
-#shuffle para ler dados de forma aleatoria. tem que estar a false para não acontecer
-train_input_data, test_input_data, train_target_data, test_target_data = train_test_split(tabela, tabela['Open'], test_size=.3, shuffle = False)
+treino_input, teste_input, treino_target, teste_target = train_test_split(tabela, tabela['Open'], test_size=.3, shuffle = False)
 
-#Treino
-train_input_data = np.array(train_input_data[1:])
-train_target_data = np.array(train_target_data[0:(len(train_target_data)-1)])
 
-#Teste
-test_input_data = np.array(test_input_data[1:])
-test_target_data = np.array(test_target_data[0:(len(test_target_data)-1)])
-train_target_data = np.reshape(train_target_data,(len(train_target_data),1))
-test_target_data = np.reshape(test_target_data,(len(test_target_data),1))
+treino_input = np.array(treino_input[0:(len(treino_input)-1)])
+treino_target = np.array(treino_target[1:])
+
+teste_input = np.array(teste_input[0:(len(teste_input)-1)])
+teste_target = np.array(teste_target[1:])
+
+treino_target = np.reshape(treino_target,(len(treino_target),1))
+teste_target = np.reshape(teste_target,(len(teste_target),1))
+
 scaler = MinMaxScaler(feature_range=(0, 1))
 
-#Normalizar
-train_input_data = scaler.fit_transform(train_input_data)
-train_target_data = scaler.fit_transform(train_target_data)
-test_input_data = scaler.fit_transform(test_input_data)
-test_target_data = scaler.fit_transform(test_target_data)
-########## FIM DO TRATAMENTO DE DADOS ########## 
+treino_input = scaler.fit_transform(treino_input)
+treino_target = scaler.fit_transform(treino_target)
+teste_input = scaler.fit_transform(teste_input)
+teste_target = scaler.fit_transform(teste_target)
+################################ FIM DO TRATAMENTO DE DADOS ################################ 
 
  
 
 
 
-########## INICIO DA FUNCAO MLPREGRESS ##########  
+################################ INICIO DA FUNCAO MLPREGRESS ################################  
 def mlp_regress(hyperparameters):
     model = MLPRegressor(activation = 'logistic', hidden_layer_sizes=(150,),learning_rate_init=(hyperparameters[0]), momentum = (hyperparameters[1]))
-    model.fit(train_input_data, train_target_data)
-    model.predict(test_input_data)
-    score = model.score(train_input_data, train_target_data)
+    model.fit(treino_input, treino_target)
+    model.predict(teste_input)
+    score = model.score(treino_input, treino_target)
     print(); print(model)
     loss = (1-score)
     print(loss)
     return loss
-########## FIM DA FUNCAO MLPREGRESS ##########  
+################################ FIM DA FUNCAO MLPREGRESS ################################  
  
 
 
-########## INICIO DA FUNCAO DE ENVIO ##########  
+################################ INICIO DA FUNCAO DE ENVIO ################################  
 def f(x):
     n_particles = x.shape[0]
     for i in [range(n_particles)]:
@@ -114,15 +75,32 @@ def f(x):
           
     j = mlp_regress(valor)
     return np.array(j)
-########## FIM DA FUNCAO DE ENVIO ##########  
+################################ FIM DA FUNCAO DE ENVIO ################################  
 
 
-########## INICIO DA CHAMADA DA FUNCAO CAT ##########   
-alh = sp.ca(10, f , 0.0001 , 0.9, 2 ,2, mr=10, smp=2,
-                 spc=False, cdc=1, srd=0.1, w=0.1, c=1.05, csi=0.6)
-########## FIM DA CHAMADA DA FUNCAO CAT ##########   
+################################ INICIO DA CHAMADA DA FUNCAO CAT ################################   
+alh = sp.ca(10, f , 0.0001 , 0.9, 2 ,2, mr=10, smp=2, spc=False, cdc=1, srd=0.1, w=0.1, c=1.05, csi=0.6)
+################################ FIM DA CHAMADA DA FUNCAO CAT ################################   
 
-print ("best score :",alh.get_Gbest())
-#print("h:",alh.get_agents())
-mlp_regress(alh.get_Gbest())
-animation(alh.get_agents(), f, 10,10)
+
+################################ INICIO PRINT RESULTADO ################################
+print ("Melhor Score :",alh.get_Gbest()) 
+################################ FIM PRINT RESULTADO ################################
+
+
+################################ INICIO GRAFICO ################################
+rc('animation', html='html5')
+# Initialize mesher with sphere function
+m = Mesher(func=fx.sphere)
+
+ 
+
+animation = plot_contour(pos_history=alh.get_agents(),
+                         mesher=m,
+                         mark=(0,0))
+
+ 
+
+HTML(animation.to_html5_video())
+
+################################ FIM GRAFICO ################################
